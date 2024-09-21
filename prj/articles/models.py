6e8f14e77bef83212ed9970a8d3249be90c88ce4
soony <dhsmf4245@gmail.com>
2024-09-21 20:06:32 +0900

    2024 09 21 TIL

--- prj/articles/models.py
+++ prj/articles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 파이썬에서 클래스 인자는 상속이다.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
